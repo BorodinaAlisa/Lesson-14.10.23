038221b2f34855aa7e72f2511beda5ec72ff526c
Алиса <@LAPTOP-QB2I6692>
2023-10-19 21:08:04 +0300

    Добавьте файлы проекта.

---
+++ Lesson 14.10.23/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_14._10._23
+{
+    internal class Program
+    {
+        enum AccountType
+        {
+            Current,
+            Savings
+        }
+        class AccountBank
+        {
+            private static int accountCounter = 1000;// статическая переменная для генерации уникального номера счета
+            private int AccountNumber;
+            private double Balance;
+            private AccountType AccountType;
+            public AccountBank()
+            {
+                AccountNumber = accountCounter;
+                accountCounter++;
+                Balance = 0; 
+                AccountType = AccountType.Current;  
+            }
+            public void SetData(int number, double balance, AccountType type)
+            {
+                AccountNumber = number;
+                Balance = balance;
+                AccountType = type;
+            }
+            public int GetAccountNumber()
+            {
+                return AccountNumber;
+            }
+            public double GetBalance()
+            {
+                return Balance;
+            }
+            public AccountType GetAccountType()
+            {
+                return AccountType;
+            }
+            public void SetBalance(double newBalance)
+            {
+                Balance = newBalance;
+            }
+            public void Deposit(double amount)
+            {
+                if (amount > 0)
+                {
+                    Balance += amount;
+                    Console.WriteLine($"Депозит на {amount} выполнен.Новый баланс: {Balance}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка!Увеличьте сумму для депозита.");
+                }
+            }
+            public void Withdraw(double amount)
+            {
+                if (amount > 0 && Balance >= amount)
+                {
+                    Balance -= amount;
+                    Console.WriteLine($"Списание {amount} выполнено.Новый баланс: {Balance}");
+                }
+                else if (amount <= 0)
+                {
+                    Console.WriteLine("Ошибка!Увеличьте сумму для списания.");
+                }
+                else
+                {
+                    Console.WriteLine("Недостаточно средств на счете.");
+                }
+            }
+
+            public void PrintAccount()
+            {
+                Console.WriteLine($"Номер счета: {AccountNumber}");
+                Console.WriteLine($"Баланс: {Balance}" );
+                Console.WriteLine($"Тип счета: {AccountType}");
+            }
+        }
+        public class Building
+        {
+            private int uniqueNumber;
+            private int Height;
+            private int Floors;
+            private int Apartments;
+            private int Entrances;
+
+            public Building(int height, int floors, int apartments, int entrances)
+            {
+                Height = height;
+                Floors = floors;
+                Apartments = apartments;
+                Entrances = entrances;
+                uniqueNumber = getNextUniqueNumber();
+            }
+            private static int nextUniqueNumber = 1;
+            public int GetUniqueNumber()
+            {
+                return uniqueNumber;
+            }
+            public int GetHeight()
+            {
+                return Height;
+            }
+            public int GetFloors()
+            {
+                return Floors;
+            }
+            public int GetApartments()
+            {
+                return Apartments;
+            }
+            public int GetEntrances()
+            {
+                return Entrances;
+            }
+            public int GetApartmentsOnFloor()
+            {
+                return Apartments / Floors;
+            }
+            public int GetApartmentsInEntrance()
+            {
+                return Apartments / Entrances;
+            }
+            public int GetFloorHeight()
+            {
+                return Height / Floors;
+            }
+            private static int getNextUniqueNumber()
+            {
+                int uniqueNumber = nextUniqueNumber;
+                nextUniqueNumber++;
+                return uniqueNumber;
+            }
+            public override string ToString()
+            {
+                return ($"Номер здания:{uniqueNumber}, высота:{Height}, этажность:{Floors}, количество квартир:{Apartments}, количество подъездов:{Entrances}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 7.1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип из упр. 3.1). Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.");
+            AccountBank accountCounter = new AccountBank();
+            accountCounter.SetData(123456789, 1000000, AccountType.Current);
+            accountCounter.PrintAccount();
+            Console.WriteLine("Упражнение 7.2.Изменить класс счет в банке из упражнения 7.1 таким образом, чтобы номер счета генерировался сам и был уникальным. Для этого надо создать в классе статическую переменную и метод, который увеличивает значение этого переменной.");
+            AccountBank account1 = new AccountBank();
+            account1.SetBalance(100000);
+            AccountBank account2 = new AccountBank();
+            account2.SetBalance(700000);
+            account1.PrintAccount();
+            account2.PrintAccount();
+            Console.WriteLine("Упражнение 7.3. Добавить в класс счет в банке два метода: снять со счета и положить на счет. Метод снять со счета проверяет, возможно ли снять запрашиваемую сумму, и в случае положительного результата изменяет баланс.");
+            account1.Deposit(777777);
+            account2.Withdraw(5050505);
+            Console.WriteLine("Домашнее задание 7.1. Реализовать класс для описания здания (уникальный номер здания, высота, этажность, количество квартир, подъездов). Поля сделать закрытыми,\r\nпредусмотреть методы для заполнения полей и получения значений полей для печати.Добавить методы вычисления высоты этажа, количества квартир в подъезде, количества квартир на этаже и т.д. Предусмотреть возможность, чтобы уникальный номер здания генерировался программно. Для этого в классе предусмотреть статическое поле, которое бы хранило последний использованный номер здания, и предусмотреть метод, который увеличивал бы значение этого поля.");
+            Building building = new Building(20, 17, 345, 7);
+            Console.WriteLine(building);
+            Console.WriteLine($"Количество квартир на этаже:{building.GetApartmentsOnFloor()}");
+            Console.WriteLine($"Количество квартир на этаже:{building.GetApartmentsInEntrance()}");
+            Console.WriteLine($"Высота этажа:{ building.GetFloorHeight()}");
+            Building building2 = new Building(30, 29, 200, 2);
+            Console.WriteLine(building2);
+            Console.WriteLine($"Количество квартир на этаже:{building2.GetApartmentsOnFloor()}");
+            Console.WriteLine($"Количество квартир на этаже:{building2.GetApartmentsInEntrance()}");
+            Console.WriteLine($"Количество квартир на этаже:{building2.GetFloorHeight()}");
+            Building building3 = new Building(30, 29, 200, 2);
+            Console.WriteLine(building3);
+            Console.WriteLine($"Количество квартир на этаже:{building3.GetApartmentsOnFloor()}");
+            Console.WriteLine($"Количество квартир на этаже:{building3.GetApartmentsInEntrance()}");
+            Console.WriteLine($"Количество квартир на этаже:{building3.GetFloorHeight()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    internal class Program
+    {
+        abstract class TableBooking// Абстрактный класс для бронирования столиков
+        {
+            public string Name {get; set;}// Свойства
+            public DateTime BookingDate {get; set;}
+            public TableBooking(string name, DateTime bookingDate)// Конструктор
+            {
+                Name = name;
+                BookingDate = bookingDate;
+            }
+            public abstract void ConfirmBooking();// Абстрактный метод
+        }
+        class TableBookingForGuests : TableBooking// Класс для бронирования столиков на определенное количество человек
+        {
+            public int NumberOfGuests {get; set;}// Свойство
+            public TableBookingForGuests(string name, DateTime bookingDate, int numberOfGuests) : base(name, bookingDate)// Конструкторы
+            {
+                NumberOfGuests = numberOfGuests;
+            }
+            public TableBookingForGuests(string name, int numberOfGuests) : this(name, DateTime.Now, numberOfGuests)// Конструкторы
+            {
+            }
+            public override void ConfirmBooking()// Методы
+            {
+                Console.WriteLine($"Столик для {NumberOfGuests} гостей забронирован на {BookingDate} для {Name}");
+            }
+            public void ChangeNumberOfGuests(int newNumberOfGuests)
+            {
+                NumberOfGuests = newNumberOfGuests;
+                Console.WriteLine($"Количество гостей для бронирования изменено на {NumberOfGuests}");
+            }
+        }
+        class TableBookingByTime : TableBooking// Класс для бронирования столиков по времени
+        {
+            public TimeSpan BookingTime {get; set;} // Свойство
+            public TableBookingByTime(string name, DateTime bookingDate, TimeSpan bookingTime) : base(name, bookingDate)// Конструкторы
+            {
+                BookingTime = bookingTime;
+            }
+            public TableBookingByTime(string name, TimeSpan bookingTime) : this(name, DateTime.Now, bookingTime)
+            {
+            }
+            public override void ConfirmBooking()// Методы
+            {
+                Console.WriteLine($"Столик забронирован на {BookingDate.ToShortDateString()} в {BookingTime} для {Name}");
+            }
+            public void ChangeBookingTime(TimeSpan newBookingTime)
+            {
+                BookingTime = newBookingTime;
+                Console.WriteLine($"Время бронирования изменено на {BookingTime}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Написать программу, тема - Бронирование столиков в ресторане");
+            Console.WriteLine("Изменение количества гостей и времени бронирования:");
+            TableBookingForGuests booking1 = new TableBookingForGuests("Колесниковa Владимира", 3);// Создание объекта класса TableBookingForGuests
+            booking1.ChangeNumberOfGuests(5);// Изменение количества гостей
+            booking1.ConfirmBooking();// Подтверждение бронирования
+            TableBookingForGuests booking2 = new TableBookingForGuests("Смирнова Олега", 2);
+            booking2.ChangeNumberOfGuests(4);
+            booking2.ConfirmBooking();
+            TableBookingByTime booking3 = new TableBookingByTime("Анашиной Дарьи", new TimeSpan(15, 30, 0));// Создание объекта класса TableBookingByTime
+            booking3.ChangeBookingTime(new TimeSpan(20, 0, 0));// Изменение времени бронирования
+            booking3.ConfirmBooking(); // Подтверждение бронирования
+            Console.ReadKey();
+        }
+    }
+}
